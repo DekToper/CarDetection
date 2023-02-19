591dcc1bd5659b6d614c5faa43d49c661f229610
DekToper <dimachumak123@gmail.com>
2023-02-19 15:23:35 +0200

    Added check not a program mode for image visualization

--- DataPreprocessing/show_images.py
+++ DataPreprocessing/show_images.py
@@ -2,11 +2,12 @@ from matplotlib import pyplot as plt
 
 
 def show_images(n, data, cfg):
-    batch = data.next()
-
     if cfg['mode'] == 'dev':
+        batch = data.next()
         fig, ax = plt.subplots(ncols=n, figsize=(20, 20))
         for idx, img in enumerate(batch[0][:n]):
             ax[idx].imshow(img.astype(int))
             ax[idx].title.set_text(('car', 'motorbike')[batch[1][idx]])
         plt.show()
+    elif cfg['mode'] == 'prod':
+        print('ModeTypeError: For image visualization, you need to switch the mode in the config file to dev mode.')
